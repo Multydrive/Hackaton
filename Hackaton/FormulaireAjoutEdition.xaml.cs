362040441b5dd8652ebde0f585c2ad6959d7f638
AndreasB <altino22@gmail.com>
2019-05-23 11:40:16 +0200

    Formulaire

---
+++ Hackaton/FormulaireAjoutEdition.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hackaton
+{
+    /// <summary>
+    /// Logique d'interaction pour FormulaireAjoutEdition.xaml
+    /// </summary>
+    public partial class FormulaireAjoutEdition : Window
+    {
+        public FormulaireAjoutEdition()
+        {
+            InitializeComponent();
+        }
+    }
+}
