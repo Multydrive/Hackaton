ce8d8ad1d4ca243738ee6c9055deeca78134fb1a
Adrien <doudou.delau@hotmail.fr>
2019-05-23 11:49:49 +0200

    Champion ajout

--- Hackaton/Champion.cs
+++ Hackaton/Champion.cs
@@ -8,6 +8,31 @@ namespace Hackaton
 {
     class Champion
     {
-        //test
+        //attributs
+        private string _nom;
+        private string _region;
+        private string _classe;
+        private string _sous_classe;
+
+        //accesseur  & mutateur
+        public string Nom { get => _nom; set => _nom = value; }
+        public string Region { get => _region; set => _region = value; }
+        public string Classe { get => _classe; set => _classe = value; }
+        public string Sous_classe { get => _sous_classe; set => _sous_classe = value; }
+
+        //constructeur
+        public Champion(string pnom, string pregion, string pclasse, string psous_classe)
+        {
+            this._nom = pnom;
+            this._region = pregion;
+            this._classe = pclasse;
+            this._sous_classe = psous_classe;
+        }
+        //Override de ToString
+        public override string ToString()
+        {
+
+            return this.Nom + "#" + this.Region + "#" + this.Classe + "#" + this.Sous_classe + "#";
+        }
     }
 }
